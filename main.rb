f8dad4dcb3d3ed72d5326579a306d15a50c430cc
kijimaD <norimaking777@gmail.com>
2022-04-29 18:18:40 +0900

    chore: initial commit

---
+++ main.rb
@@ -0,0 +1,106 @@
+def _eval(exp)
+  if not list?(exp)
+    if immediate_val?(exp)
+      exp
+    else
+      lookup_primitive_fun(exp)
+    end
+  else
+    fun = _eval(car(exp))
+    args = eval_list(cdr(exp))
+    apply(fun, args)
+  end
+end
+
+def list?(exp)
+  exp.is_a?(Array)
+end
+
+def lookup_primitive_fun(exp)
+  $primitive_fun_env[exp]
+end
+
+$primitive_fun_env = {
+  :+ => [:prim, lambda{ |x, y| x + y }],
+  :- => [:prim, lambda{ |x, y| x - y }],
+  :* => [:prim, lambda{ |x, y| x * y }],
+}
+
+def car(list)
+  list[0]
+end
+
+def cdr(list)
+  list[1..-1]
+end
+
+def eval_list(exp)
+  exp.map{ |e| _eval(e)}
+end
+
+def immediate_val?(exp)
+  num?(exp)
+end
+
+def num?(exp)
+  exp.is_a?(Numeric)
+end
+
+def apply(fun, args)
+  apply_primitive_fun(fun, args)
+end
+
+def apply_primitive_fun(fun, args)
+  fun_val = fun[1]
+  fun_val.call(*args)
+end
+
+# puts _eval([:+, 1, 2])
+
+def lookup_var(var, env)
+  alist = env.find{ |alist| alist.key?(var) }
+  if alist == nil
+    raise "could not find value to variables: '#{var}'"
+  end
+  alist[var]
+end
+
+def extend_env(parameters, args, env)
+  alist = parameters.zip(args)
+  h = Hash.new
+  alist.each { |k, v| h[k] = v| }
+  [h] + env
+end
+
+def eval_let(exp, env)
+  parameters, args, body = let_to_parameters_args_body(exp)
+  new_exp = [[:lambda, parameter, body]] + args
+  _eval(new_exp, env)
+end
+
+def let_to_parameters_args_body(exp)
+  [exp[1].map{ |e| e[0]}, exp[1].map{ |e| e[1]}, exp[2]]
+end
+
+def let?(exp)
+  exp[0] == :let
+end
+
+def eval_lambda(exp, env)
+  make_closure(exp, env)
+end
+
+def make_closure(exp, env)
+  parameters, body = exp[1], exp[2]
+  [:closure, parameters, body, env]
+end
+
+def lambda_apply(closure, args)
+  parameters, body, env = closure_to_parameters_body_env(closure)
+  new_env = extend_env(parameters.args, env)
+  _eval(body, new_env)
+end
+
+def closure_to_parameters_body_env(closure)
+  [closure[1], closure[2], closure[3]]
+end
