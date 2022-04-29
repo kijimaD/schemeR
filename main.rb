b1cff44fa014027726af8ba0266728d69b1630fa
kijimaD <norimaking777@gmail.com>
2022-04-29 19:14:33 +0900

    feat: add env(chapter2)

--- main.rb
+++ main.rb
@@ -1,15 +1,47 @@
-def _eval(exp)
+$primitive_fun_env = {
+  :+ => [:prim, lambda{ |x, y| x + y }],
+  :- => [:prim, lambda{ |x, y| x - y }],
+  :* => [:prim, lambda{ |x, y| x * y }],
+}
+
+$global_env = [$primitive_fun_env]
+
+def _eval(exp, env)
   if not list?(exp)
     if immediate_val?(exp)
       exp
     else
-      lookup_primitive_fun(exp)
+      lookup_var(exp, env)
     end
   else
-    fun = _eval(car(exp))
-    args = eval_list(cdr(exp))
-    apply(fun, args)
+    if special_form?(exp)
+      eval_special_form(exp, env)
+    else
+      fun = _eval(car(exp), env)
+      args = eval_list(cdr(exp), env)
+      apply(fun, args)
+    end
   end
+end
+
+def special_form?(exp)
+  lambda?(exp) or let?(exp)
+end
+
+def lambda?(exp)
+  exp[0] == :lambda
+end
+
+def eval_special_form(exp, env)
+  if lambda?(exp)
+    eval_lambda(exp, env)
+  elsif let?(exp)
+    eval_let(exp, env)
+  end
+end
+
+def eval_list(exp, env)
+  exp.map{ |e| _eval(e, env) }
 end
 
 def list?(exp)
@@ -20,22 +52,12 @@ def lookup_primitive_fun(exp)
   $primitive_fun_env[exp]
 end
 
-$primitive_fun_env = {
-  :+ => [:prim, lambda{ |x, y| x + y }],
-  :- => [:prim, lambda{ |x, y| x - y }],
-  :* => [:prim, lambda{ |x, y| x * y }],
-}
-
 def car(list)
   list[0]
 end
 
 def cdr(list)
   list[1..-1]
-end
-
-def eval_list(exp)
-  exp.map{ |e| _eval(e)}
 end
 
 def immediate_val?(exp)
@@ -47,15 +69,21 @@ def num?(exp)
 end
 
 def apply(fun, args)
-  apply_primitive_fun(fun, args)
+  if primitive_fun?(fun)
+    apply_primitive_fun(fun, args)
+  else
+    lambda_apply(fun, args)
+  end
+end
+
+def primitive_fun?(exp)
+  exp[0] == :prim
 end
 
 def apply_primitive_fun(fun, args)
   fun_val = fun[1]
   fun_val.call(*args)
 end
-
-# puts _eval([:+, 1, 2])
 
 def lookup_var(var, env)
   alist = env.find{ |alist| alist.key?(var) }
@@ -68,7 +96,7 @@ end
 def extend_env(parameters, args, env)
   alist = parameters.zip(args)
   h = Hash.new
-  alist.each { |k, v| h[k] = v| }
+  alist.each { |k, v| h[k] = v }
   [h] + env
 end
 
@@ -97,10 +125,13 @@ end
 
 def lambda_apply(closure, args)
   parameters, body, env = closure_to_parameters_body_env(closure)
-  new_env = extend_env(parameters.args, env)
+  new_env = extend_env(parameters, args, env)
   _eval(body, new_env)
 end
 
 def closure_to_parameters_body_env(closure)
   [closure[1], closure[2], closure[3]]
 end
+
+exp = [[:lambda, [:x, :y], [:+, :x, :y]], 3, 2]
+p _eval(exp, $global_env)
