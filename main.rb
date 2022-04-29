8f484392f2eed98c72c01a608564a025becd0f3b
kijimaD <norimaking777@gmail.com>
2022-04-29 23:13:51 +0900

    feat: repl(done)

--- main.rb
+++ main.rb
@@ -302,6 +302,60 @@ def quote?(exp)
   exp[0] == :quote
 end
 
+def repl
+  prompt = '>>> '
+  second_prompt = '> '
+  while true
+    print prompt
+    line = gets or return
+    while line.count('(') > line.count(')')
+      print second_prompt
+      next_line = gets or return
+      line += next_line
+    end
+    redo if line =~ /\A\s*\z/m
+    begin
+      val = _eval(parse(line), $global_env)
+    rescue Exception => e
+      puts e.to_s
+      redo
+    end
+    puts pp(val)
+  end
+end
+
+def closure?(exp)
+  exp[0] == :closure
+end
+
+def pp(exp)
+  if exp.is_a?(Symbol) or num?(exp)
+    exp.to_s
+  elsif exp == nil
+    'nil'
+  elsif exp.is_a?(Array) and closure?(exp)
+    parameter, body, env = exp[1], exp[2], exp[3]
+    "(closure #{pp(parameter)} #{pp(body)})"
+  elsif exp.is_a?(Array) and lambda?(exp)
+    parameters, body = exp[1], exp[2]
+    "(lambda #{pp(parameters)} #{pp(body)})"
+  elsif exp.is_a?(Hash)
+    if exp == $primitive_fun_env
+      '*prinmitive_fun_env*'
+    elsif exp == $boolean_env
+      '*boolean_env*'
+    elsif exp == $list_env
+      '*list_env*'
+    else
+      '{' + exp.map{|k, v| pp(k) + ':' + pp(v)}.join(', ') + '}'
+    end
+  elsif exp.is_a?(Array)
+    '(' + exp.map{|e| pp(e)}.join(', ') + ')'
+  else
+    exp.to_s
+  end
+end
+
 def parse(exp)
   program = exp.strip().
               gsub(/[a-zA-Z\+\-\*><=][0-9a-zA-Z\+\-=!*]*/, ':\\0').
@@ -367,7 +421,11 @@ end
 
 # ================
 
-exp = parse('(define (length list) (if (null? list) 0 (+ (length (cdr list)) 1)))')
-execute(exp)
-exp = parse('(length (quote (1 2 3)))')
-execute(exp)
+# exp = parse('(define (length list) (if (null? list) 0 (+ (length (cdr list)) 1)))')
+# execute(exp)
+# exp = parse('(length (quote (1 2 3)))')
+# execute(exp)
+
+# ================
+
+repl
